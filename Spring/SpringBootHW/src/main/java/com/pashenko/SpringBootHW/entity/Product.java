a4625729a2b92500f7ebc4649bd5e5100427ac89
Andrew <andrew.p52@yandex.ru>
2021-08-28 10:04:07 +0300

    Homework 4 Spring Boot

---
+++ Spring/SpringBootHW/src/main/java/com/pashenko/SpringBootHW/entity/Product.java
@@ -0,0 +1,34 @@
+package com.pashenko.SpringBootHW.entity;
+
+import java.math.BigDecimal;
+
+
+public class Product {
+    private int id;
+    private String title;
+    private BigDecimal cost;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+}
