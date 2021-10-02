668d65d7ff6e3af2d5e7d105f22c1b9ece9def7d
Andrew <andrew.p52@yandex.ru>
2021-10-02 14:31:05 +0300

    User`s profile retrieving and showing Minor "front" additions

--- Spring/eshop/src/main/java/com/pashenko/eshop/controllers/AdminController.java
+++ Spring/eshop/src/main/java/com/pashenko/eshop/controllers/AdminController.java
@@ -1,11 +1,12 @@
 package com.pashenko.eshop.controllers;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Secured("ROLE_ADMIN")
 @RequestMapping("/admin")
 public class AdminController {
 
--- Spring/eshop/src/main/java/com/pashenko/eshop/services/UserService.java
+++ Spring/eshop/src/main/java/com/pashenko/eshop/services/UserService.java
@@ -5,13 +5,17 @@ import com.pashenko.eshop.entities.User;
 import com.pashenko.eshop.repositories.RoleRepository;
 import com.pashenko.eshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.security.Principal;
 import java.util.Collection;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -55,4 +59,9 @@ public class UserService implements UserDetailsService {
         return userRepository.save(u);
     }
 
+    public User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername();
+        return this.userRepository.getFirstByLogin(name);
+    }
 }
