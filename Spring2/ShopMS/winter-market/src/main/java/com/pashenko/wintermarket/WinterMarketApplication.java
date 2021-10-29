ad6f7bb9ae91b7ad801e3a2ef1ccaeee316149aa
Andrew <andrew.p52@yandex.ru>
2021-10-29 14:04:28 +0300

    Homework 5 ( Working winter market (fetches config from server, registers in eureka) )

--- Spring2/ShopMS/winter-market/src/main/java/com/pashenko/wintermarket/WinterMarketApplication.java
+++ Spring2/ShopMS/winter-market/src/main/java/com/pashenko/wintermarket/WinterMarketApplication.java
@@ -1,4 +1,13 @@
 package com.pashenko.wintermarket;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
 public class WinterMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WinterMarketApplication.class, args);
+    }
 }
